90b32091d6543069c46d479eda7c17f6d1cea25f
Jolene Ie <ji92@cornell.edu>
2025-11-06 18:37:56 -0500

    fixed docker

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ sys.modules['__main__'].to_str_func = to_str_func
 load_dotenv(find_dotenv())
 
 b = pins.board_gcs('info-4940-models/ji92/', allow_pickle_read=True)
-v = VetiverModel.from_pin(b, 'prediabetes-model-2', version = '20251106T182318Z-51f55')
+v = VetiverModel.from_pin(b, 'prediabetes-model-3', version = '20251106T183718Z-51f55')
 
 vetiver_api = vetiver.VetiverAPI(v)
 api = vetiver_api.app
