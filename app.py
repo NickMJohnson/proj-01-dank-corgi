cfe0b2d729baa07bee9d0e46182f2618a20b0d7d
Jolene Ie <ji92@cornell.edu>
2025-11-06 14:49:22 -0500

    fixed api maybe

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import pins
 load_dotenv(find_dotenv())
 
 b = pins.board_gcs("info-4940-models/ji92/", allow_pickle_read=True)
-PIN_NAME = "nhanes-prediabetes-py"
-PIN_VERSION = "20251105T205309Z-32e04"
+PIN_NAME = "nhanes-prediabetes-new"
+PIN_VERSION = "20251106T143424Z-269c8"
 
 v = vetiver.VetiverModel.from_pin(b, PIN_NAME, version=PIN_VERSION)
 api = vetiver.VetiverAPI(v, check_prototype=False)
