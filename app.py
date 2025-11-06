c9632ad7e4913314be76eb4d91d9526fe6e1b442
Jolene Ie <ji92@cornell.edu>
2025-11-06 15:59:02 -0500

    hash change

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ load_dotenv(find_dotenv())
 
 b = pins.board_gcs("info-4940-models/ji92/", allow_pickle_read=True)
 PIN_NAME = "nhanes-prediabetes-new"
-PIN_VERSION = "20251106T143424Z-269c8"
+PIN_VERSION = "20251106T155605Z-cbe41"
 
 v = vetiver.VetiverModel.from_pin(b, PIN_NAME, version=PIN_VERSION)
 api = vetiver.VetiverAPI(v, check_prototype=False)
